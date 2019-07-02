936928d349a8edbae3b9d4efbc2c9a12ed9f459c
George Ruiz <georuiz817@gmail.com>
2019-07-02 16:03:48 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,17 @@
+class Patient
+  attr_accessor :name, :appointments
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(doctor, date)
+    Appointment.new(self, doctor, date)
+  end
+end
